b21adbed72ebcbe7c2f3a8ed91704dca934647d2
Marc Radziwill <hallo@marcradziwill.com>
2023-01-12 16:01:31 +0100

    webc in footer

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,12 +1,12 @@
+const glob = require('fast-glob');
+const path = require('path');
+
 const { EleventyRenderPlugin } = require("@11ty/eleventy");
-const i18nPlugin = require('eleventy-plugin-i18n');
 const webcPlugin = require('@11ty/eleventy-plugin-webc');
-const translations = require('./src/_data/i18n/translations');
 
 const config = {
     dir: {
         input: 'src',
-        // assets: 'assets',
         output: '_site',
     },
     passthroughFileCopy: true,
@@ -14,17 +14,13 @@ const config = {
 }
 
 module.exports = function (eleventyConfig) {
+
     // Plugins
     eleventyConfig.addPlugin(EleventyRenderPlugin);
     eleventyConfig.addPlugin(webcPlugin, {
 		components: "src/_includes/components/webc/**/*.webc",
 	});
-    eleventyConfig.addPlugin(i18nPlugin, {
-        translations,
-        fallbackLocales: {
-            'en': 'en'
-        }
-    });
+    
     eleventyConfig.addLayoutAlias('base', 'layouts/base.njk');
 
     return config;
